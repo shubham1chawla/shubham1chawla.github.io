97b605657f394846092c5d50a49c8fa08550933f
Shubham Chawla <shubham_chawla@live.com>
2018-06-20 07:49:08 +0530

    Added Projects

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 // CONSTANTS
-const TOP_BOTTOM_MARGIN_MAIN_RESIZE_ELEMENT_IN_PERCENTAGE = 0.12;
-const MAIN_HEIGHT_RESIZE_ELEMENT_IN_PERCENTAGE = 0.76;
+const TOP_BOTTOM_MARGIN_MAIN_RESIZE_ELEMENT_IN_PERCENTAGE = 0.10;
+const MAIN_HEIGHT_RESIZE_ELEMENT_IN_PERCENTAGE = 0.8;
 
-const LEFT_RIGHT_MARGIN_MAIN_RESIZE_ELEMENT_IN_PERCENTAGE = 0.15;
-const MAIN_WIDTH_RESIZE_ELEMENT_IN_PERCENTAGE = 0.7;
+const LEFT_RIGHT_MARGIN_MAIN_RESIZE_ELEMENT_IN_PERCENTAGE = 0.1;
+const MAIN_WIDTH_RESIZE_ELEMENT_IN_PERCENTAGE = 0.8;
 
 // Getting some variables
 
@@ -54,7 +54,7 @@ function resize(){
 var currently_visible_page = "home";
 var tab_data = [
 	{name: "about", data: "<div class='container-fuild'><h3 class='raleway_font'><i class='fas fa-user-circle'></i> About Me</h3><hr><div class='row raleway_font'><div class='col-md-6'><strong><i class='fas fa-user-graduate'></i> EDUCATION</strong><ul><li>B.E. in Electronics & Communications<br><span style='font-size: 0.8em;'>Thapar Institute of Engineering & Technology<br>2015 - Present</span></li><li>Class XII<br><span style='font-size: 0.8em;'>Modern Public School, Bhiwadi<br>2014 – 2015</span></li><li>Class X<br><span style='font-size: 0.8em;'>Modern Public School, Bhiwadi<br>2012 – 2013</span></li></ul></div><div class='col-md-6'><strong><i class='fas fa-basketball-ball'></i> INTERESTS</strong><ul><li>Technology & Gadgets</li><li>Mentoring Juniors</li><li>Swimming</li><li>Chess</li></ul><strong><i class='fas fa-language'></i> LANGUAGES</strong><ul><li>English<br><p class='text-muted'>Professional</p></li><li>Hindi<br><p class='text-muted'>Native</p></li></ul></div></div></div>"},
-	{name: "projects", data: "<div class='container-fuild'><h3 class='raleway_font'>Projects</h3><hr><div class='row raleway_font'><div class='col-md-6'><strong><i class='fas fa-book-open'></i> EDUCATION</strong><ul><li>B.E. in Electronics & Communications<br><span style='font-size: 0.8em;'>Thapar Institute of Engineering & Technology<br>2015 - Present</span></li><li>Class XII<br><span style='font-size: 0.8em;'>Modern Public School, Bhiwadi<br>2014 – 2015</span></li><li>Class X<br><span style='font-size: 0.8em;'>Modern Public School, Bhiwadi<br>2012 – 2013</span></li></ul></div><div class='col-md-6'><strong><i class='fas fa-basketball-ball'></i> INTERESTS</strong><ul><li>Technology & Gadgets</li><li>Mentoring Juniors</li><li>Swimming</li><li>Chess</li></ul><strong><i class='fas fa-language'></i> LANGUAGES</strong><ul><li>English<br><p class='text-muted'>Professional</p></li><li>Hindi<br><p class='text-muted'>Native</p></li></ul></div></div></div>"}
+	{name: "projects", data: "<div class='container-fuild' style='height: 90%; overflow-y: scroll; overflow-x: hidden'><h3 class='raleway_font'><i class='fas fa-project-diagram'></i> Projects</h3><hr><ul><li><div class='raleway_font'><strong>Project Sales Simulation</strong><div class='row'><div class='col-md-6'><p style='text-align: justify;'>This project aimed to create a real-life (almost) analysis of how a product would sell in an Indian market comprising various types of population. This was a helper project in my course Innovation & Entrepreneurship during semester 6 (Q1-2 2018). This python based project used Matplot Lib to plot the results obtained as day-wise & sales-wise graphs. Other than that it exports the data in a usable CSV format.Further details are listed at the GitHub link attached along.</p></div><div class='col-md-6'><img src='img/product_sales.png' alt='Product-Simulation' width='100%' height='auto'><br><br><b>TECHNOLOGY USED</b><pre>python</pre><b>TIMELINE</b><p>Feb 2018 – May 2018</p><b>LINK</b><pre>https://github.com/shubham1chawla/Product-Simulation</pre></div></div></div></li><li><div class='raleway_font'><strong>The Strategists</strong><div class='row'><div class='col-md-6'><p style='text-align: justify;'>The Strategists is a monopoly based online multiplayer game based on the concepts of Foreign Direct Investment. It involves buying, trading lands in percentages and deliver a fun gameplay. The project used PHP as the backend, JS with Bootstrap as frontend along with heavy use of AJAX.</p></div><div class='col-md-6'><img src='img/thestrategists.png' alt='Product Sales pic' height='auto' width='100%'><br><br><b>TECHNOLOGY USED</b><pre>HTML, CSS, PHP, bootstrap</pre><b>TIMELINE</b><p>Mar 2018 – Apr 2018</p><b>LINK</b><pre>http://thestrategists.000webhostapp.com</pre></div></div></div></li><li><div class='raleway_font'><strong>Room Manager</strong><div class='row'><div class='col-md-6'><p style='text-align: justify;'>Room Manager is a simple backend supported webapp that uses fundamentals of SQL, PHP and Bootstrap to implement a room managing app with ability to book rooms with different user levels.</p></div><div class='col-md-6'><b>TECHNOLOGY USED</b><pre>HTML, CSS, PHP, bootstrap</pre><b>TIMELINE</b><p>Jan 2018 – Feb 2018</p><b>LINK</b><pre>https://github.com/shubham1chawla/roomManager</pre></div></div></div></li><li><div class='raleway_font'><strong>Tennis Game</strong><div class='row'><div class='col-md-6'><p style='text-align: justify;'>Tennis Game uses core JAVA to simulate an air-hockey sort of game. CORE java used to handle different threads and other game physics to give realistic gameplay.</p></div><div class='col-md-6'><b>TECHNOLOGY USED</b><pre>JAVA</pre><b>TIMELINE</b><p>July 2017 – Aug 2017</p><b>LINK</b><pre>https://github.com/shubham1chawla/Tennis_Game</pre></div></div></div></li></ul></div>"}
 ];
 
 function managePage(){
@@ -64,24 +64,29 @@ function managePage(){
 			$("#inject").html("");
 			$("#inject").css("display", "none");
 			$("#home_div").css("display", "block");
+			$("#resize_element").css("opacity", 0.9);
 		break;
 		case "about":
 			$("#home_div").css("display", "none");
 			$("#inject").html(tab_data[0].data);
 			$('#inject').css("display", "block");
+			$("#resize_element").css("opacity", 1);
 		break;
 		case "projects":
 			$("#home_div").css("display", "none");
 			$("#inject").html(tab_data[1].data);
 			$('#inject').css("display", "block");
+			$("#resize_element").css("opacity", 1);
 		break;
 		case "experiences":
 			$("#home_div").css("display", "none");
 			$("#inject").html("experiences");
+			$("#resize_element").css("opacity", 1);
 		break;
 		case "skills":
 			$("#home_div").css("display", "none");
 			$("#inject").html("skills");
+			$("#resize_element").css("opacity", 1);
 		break;
 	}
 }
