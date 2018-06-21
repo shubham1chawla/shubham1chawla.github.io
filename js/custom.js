d6518b5b8dca261a81b105cfd71a4733a946eb1e
Shubham Chawla <shubham_chawla@live.com>
2018-06-22 02:56:05 +0530

    Completed

--- js/custom.js
+++ js/custom.js
@@ -1,131 +1,3 @@
-/*
-	DATA
-*/
-
-var tab_data = [
-	{name: "about", data: "<div class='container-fuild'><h3 class='raleway_font'><i class='fas fa-user-circle'></i> About Me</h3><hr><div class='row raleway_font' style='padding-left: 5%; padding-right: 5%;'><div class='col-md-6'><strong><i class='fas fa-user-graduate'></i> EDUCATION</strong><ul><li>B.E. in Electronics & Communications<br><span style='font-size: 0.8em;'>Thapar Institute of Engineering & Technology<br>2015 - Present</span></li><li>Class XII<br><span style='font-size: 0.8em;'>Modern Public School, Bhiwadi<br>2014 – 2015</span></li><li>Class X<br><span style='font-size: 0.8em;'>Modern Public School, Bhiwadi<br>2012 – 2013</span></li></ul></div><div class='col-md-6'><strong><i class='fas fa-basketball-ball'></i> INTERESTS</strong><ul><li>Technology & Gadgets</li><li>Mentoring Juniors</li><li>Swimming</li><li>Chess</li></ul><strong><i class='fas fa-language'></i> LANGUAGES</strong><ul><li>English<br><p class='text-muted'>Professional</p></li><li>Hindi<br><p class='text-muted'>Native</p></li></ul></div></div></div>"},
-	{name: "projects", data: [
-		{
-			title: "Project Sales Simulation",
-			img: "product_sales.png",
-			text: "This project aimed to create a real-life (almost) analysis of how a product would sell in an Indian market comprising various types of population. This was a helper project in my course Innovation & Entrepreneurship during semester 6 (Q1-2 2018). This python based project used Matplot Lib to plot the results obtained as day-wise & sales-wise graphs. Other than that it exports the data in a usable CSV format.Further details are listed at the GitHub link attached along.",
-			tech: "python",
-			timeline: "Feb 2018 – May 2018",
-			link: "https://github.com/shubham1chawla/Product-Simulation"
-		},
-		{
-			title: "The Strategists",
-			img: "thestrategists.png",
-			text: "The Strategists is a monopoly based online multiplayer game based on the concepts of Foreign Direct Investment. It involves buying, trading lands in percentages and deliver a fun gameplay. The project used PHP as the backend, JS with Bootstrap as frontend along with heavy use of AJAX.",
-			tech: "HTML, CSS, PHP, bootstrap",
-			timeline: "Mar 2018 – Apr 2018",
-			link: "http://thestrategists.000webhostapp.com"
-		},
-		{
-			title: "Room Manager",
-			img: "room_manager.png",
-			text: "Room Manager is a simple backend supported webapp that uses fundamentals of SQL, PHP and Bootstrap to implement a room managing app with ability to book rooms with different user levels.",
-			tech: "HTML, CSS, PHP, bootstrap",
-			timeline: "Jan 2018 – Feb 2018",
-			link: "https://github.com/shubham1chawla/roomManager"
-		},
-		{
-			title: "Tennis Game",
-			img: "tennis_game.jpg",
-			text: "Tennis Game uses core JAVA to simulate an air-hockey sort of game. CORE java used to handle different threads and other game physics to give realistic gameplay.",
-			tech: "JAVA",
-			timeline: "July 2017 – Aug 2017",
-			link: "https://github.com/shubham1chawla/Tennis_Game"
-		},
-		{
-			title: "Tic Tac Toe",
-			img: "tictactoe.png",
-			text: "This project was my first step to game development. Implementing TicTacToe gameplay on Terminal with a smart Computer AI that forms dynamic move-table in order to play against you. Project emphasis on how the computer will react to users moves and also predict where to place its turn using complex algorithms.",
-			tech: "JAVA",
-			timeline: "May 2017 – Jun 2017",
-			link: "https://github.com/shubham1chawla/TicTacToe"
-		}
-	]},
-	{name: "skills", data: [
-		{
-			title: "HTML 5",
-			icon: "<i class='fab fa-html5 fa-5x'></i>",
-			level: 5
-		},
-		{
-			title: "CSS 3",
-			icon: "<i class='fab fa-css3-alt fa-5x'></i>",
-			level: 3
-		},
-		{
-			title: "JavaScript",
-			icon: "<i class='fab fa-js-square fa-5x'></i>",
-			level: 4
-		},
-		{
-			title: "Bootstrap",
-			icon: "<img src='img/B_black_white.svg' width='80px' height='80px' style='border-radius: 10%; opacity: 0.9;'>",
-			level: 4
-		},
-		{
-			title: "Jquery",
-			icon: "<img src='img/jquery-filled.png' width='80px' height='80px; opacity: 0.9;'>",
-			level: 3
-		},
-		{
-			title: "PHP",
-			icon: "<i class='fab fa-php fa-5x'></i>",
-			level: 4
-		},
-		{
-			title: "MySQL",
-			icon: "<img src='img/mysql-logo-filled.png' width='80px' height='80px'; opacity: 0.9;>",
-			level: 5
-		},
-		{
-			title: "C++",
-			icon: "<img src='img/c-plus-plus-logo-filled.png' width='80px' height='80px; opacity: 0.9;'>",
-			level: 4
-		},
-		{
-			title: "Python",
-			icon: "<i class='fab fa-python fa-5x'></i>",
-			level: 3
-		},
-		{
-			title: "JAVA",
-			icon: "<i class='fab fa-java fa-5x'></i>",
-			level: 3
-		},
-		{
-			title: "ARMv7",
-			icon: "<i class='fas fa-microchip fa-5x'></i>",
-			level: 3
-		},
-		{
-			title: "Adobe Photoshop",
-			icon: "<img src='img/ps.jpg' width='80px' height='80px; opacity: 0.9;'>",
-			level: 5
-		},
-		{
-			title: "Adobe Illustrator",
-			icon: "<img src='img/ai.jpg' width='80px' height='80px; opacity: 0.9;'>",
-			level: 4
-		},
-		{
-			title: "Adobe Premiere Pro",
-			icon: "<img src='img/ppro.png' width='80px' height='80px; opacity: 0.9;'>",
-			level: 4
-		},
-		{
-			title: "Adobe Indesign",
-			icon: "<img src='img/in.jpg' width='80px' height='80px; opacity: 0.9;'>",
-			level: 4
-		}
-	]}
-];
-
-
 // CONSTANTS
 const TOP_BOTTOM_MARGIN_MAIN_RESIZE_ELEMENT_IN_PERCENTAGE = 0.10;
 const MAIN_HEIGHT_RESIZE_ELEMENT_IN_PERCENTAGE = 0.8;
@@ -143,29 +15,23 @@ var doc_height = $(document).height();
 var resize_element = document.querySelector('#resize_element');
 var highfyLines = document.querySelector('#highfyLines');
 
-var highfyLinesData = [
-	{name: "a Full Stack Developer"},
-	{name: "a UI/UX Designer"},
-	{name: "a Food <i class='fas fa-heart'></i>er"},
-	{name: "a proud Indian"},
-	{name: "a Freelancer"}
-];
-
 window.onload = function(){
 	//resizing the div
 	setInterval(resize, 1000/60);
 
 	//To change content
-	setInterval(changeTagLine, 3000);
+	setInterval(changeTagLine, 2500);
 };
+
+/*
+	RESIZE MECH
+*/
 
 function resize(){
 	var viewport_height = $(window).height();
 	var viewport_width = $(window).width();
 	var doc_width = $(document).width();
 	var doc_height = $(document).height();
-
-	// console.log(doc_width+"|"+doc_height+"|"+viewport_width+"|"+viewport_height);
 
 	//Calculating height from top and total visible area
 	var margin_top_bottom = viewport_height*TOP_BOTTOM_MARGIN_MAIN_RESIZE_ELEMENT_IN_PERCENTAGE;
@@ -179,6 +45,10 @@ function resize(){
 	$("#inject").css("height", (div_height-30)+"px");
 }
 
+/*
+	MANAGE PAGES
+*/
+
 var currently_visible_page = "home";
 function managePage(){
 	toggle_slider();
@@ -191,53 +61,30 @@ function managePage(){
 		break;
 		case "about":
 			$("#home_div").css("display", "none");
-			$("#inject").html(tab_data[0].data);
+			build_about_page();
 			$('#inject').css("display", "block");
-			$("#resize_element").css("opacity", 1);
+			$("#resize_element").css("opacity", 0.9);
 		break;
 		case "projects":
 			$("#home_div").css("display", "none");
-			$('#inject').css("display", "block");
 			build_project_page();
-			$("#resize_element").css("opacity", 1);
+			$('#inject').css("display", "block");
+			$("#resize_element").css("opacity", 0.9);
 		break;
 		case "experiences":
 			$("#home_div").css("display", "none");
+			build_experience_page();
 			$('#inject').css("display", "block");
-			$("#inject").html("experiences");
-			$("#resize_element").css("opacity", 1);
+			$("#resize_element").css("opacity", 0.9);
 		break;
 		case "skills":
 			$("#home_div").css("display", "none");
-			$('#inject').css("display", "block");
 			build_skill_page();
+			$('#inject').css("display", "block");
 			$("#resize_element").css("opacity", 0.9);
 		break;
 	}
 }
-
-
-
-
-/*
-	CHANGE TAGLINE MECHANISM
-*/
-var tagline_num = 0;
-function changeTagLine(){
-	tagline_num++;
-	if(tagline_num >= highfyLinesData.length){
-		tagline_num = 0;
-	}
-	highfyLines.innerHTML = highfyLinesData[tagline_num].name;
-}
-
-
-
-
-
-/*
-	NAVBAR LINKS
-*/
 
 $("#home").click(function(){
 	currently_visible_page = "home";
@@ -264,16 +111,18 @@ $("#skills").click(function(){
 	managePage();
 });
 
+/*
+	CHANGE TAGLINE MECHANISM
+*/
 
-
-
-
-
-
-
-
-
-
+var tagline_num = 0;
+function changeTagLine(){
+	tagline_num++;
+	if(tagline_num >= tab_data[4].data.length){
+		tagline_num = 0;
+	}
+	highfyLines.innerHTML = tab_data[4].data[tagline_num].name;
+}
 
 /*
 	THIS SECTION IS FOR EXPANDING THE NAV BAR
@@ -301,15 +150,6 @@ function toggle_slider(){
 	slider_toggled = false;
 	return true;
 }
-
-
-
-
-
-
-
-
-
 
 /*
 	THIS SECTION IS FOR SOCIAL LINK HOVERING
@@ -366,47 +206,42 @@ $("#github").hover(function(){
 });
 
 
-
-
-
-
 /*
 	MODAL ACTION & PROJECT PAGE BUILDER
 */
 
 function build_project_page(){
-	var page_html = "<div class='container-fluid'><h3 class='raleway_font'><i class='fas fa-project-diagram'></i> Projects</h3><hr>";
+	var page_html = "<h3 class='raleway_font'><i class='fas fa-project-diagram'></i> Projects</h3><hr><div class='container-fluid' style='overflow-y: scroll;'>";
 	page_html += "<div class='row'>";
 	var projects = tab_data[1].data;
 	for(var i=0; i<projects.length; i++){
 		page_html += "<div class='col-md-3'>";
 		page_html += "<button id='"+i+"' class='btn btn-default project' style='background-color: rgba(0,0,0,0);' data-toggle='modal' data-target='#myModal' alt='"+i+"'>";
 		page_html += "<div class='con'>";
-		page_html += "<img src='img/"+projects[i].img+"' alt='"+projects[i].title+"' width='100%' height='200px' class='image'>";
+		page_html += "<img src='img/"+projects[i].img+"' alt='"+projects[i].title+"' width='100%' height='100%' class='image'>";
 		page_html += "<span class='middle raleway_font'><strong>"+projects[i].title+"<strong></span>"
 		page_html += "</div>";
 		page_html += "</button>";
 		page_html += "</div>";
 	}
-	page_html += "</div>"
+	page_html += "</div>";
 	$("#inject").html(page_html);
-	$(".project").click(make_modal);
+	$(".project").click(make_project_modal);
 }
 
-$(".project").click(make_modal);
-function make_modal(){
+$(".project").click(make_project_modal);
+function make_project_modal(){
 	var id = $(this).attr("id");
 	var project_details = tab_data[1].data[id];
 	$("#modal_heading").html(project_details.title);
 	var body_html = "";
-	body_html += "<strong>SCREENSHOTS</strong><hr><img src='img/"+project_details.img+"' width='100%' height='auto'><br>";
+	body_html += "<img src='img/"+project_details.img+"' width='100%' height='auto'><br>";
 	body_html += "<hr><strong>DESCRIPTION</strong><hr><p style='text-align: justify;'>"+project_details.text+"</p>";
 	body_html += "<hr><strong>TECHNOLOGY USED</strong><hr><pre>"+project_details.tech+"</pre>";
 	body_html += "<hr><strong>TIMELINE</strong><hr>"+project_details.timeline;
 	body_html += "<hr><strong>LINK</strong><hr><a href='"+project_details.link+"'><pre>"+project_details.link+"</pre></a>";
 	$("#modal_body").html(body_html);
 }
-
 
 /*
 	SKILL PAGE BUILDER
@@ -415,20 +250,102 @@ function make_modal(){
 function build_skill_page(){
 	var page_html = "<div class='container-fluid'><h3 class='raleway_font'><i class='fas fa-code'></i> Skills & Proficiency</h3><hr>";
 	page_html += "<div class='row text-center' style='margin-bottom: 30px;'>";
-	var skills = tab_data[2].data;
+	var skills = tab_data[3].data;
 	for(var i=0; i<skills.length; i++){
 		page_html += "<div class='col-md-4 col-xs-6' title='Skill Level: "+skills[i].level+"/5'>";
 		page_html += "<br>"+skills[i].icon+"<br><span><strong>"+skills[i].title+"</strong></span><br>";
 		for(var j=0; j<5; j++){
 			if(j<skills[i].level){
-				page_html += "<i class='fas fa-circle'></i>";
+				page_html += "<i class='fas fa-circle' style='margin-left:2px;margin-right:2px;font-size:0.6em;'></i>";
 			}
 			else{
-				page_html += "<i class='far fa-circle'></i>";
+				page_html += "<i class='far fa-circle' style='margin-left:2px;margin-right:2px;font-size:0.6em;'></i>";
 			}
 		}
 		page_html += "<br></div>";
 	}
-	page_html += "</div>"
+	page_html += "</div>";
 	$("#inject").html(page_html);
+}
+
+/*
+	ABOUT PAGE BUILDER
+*/
+
+function build_about_page(){
+	var page_html = "<div class='container-fluid'><h3 class='raleway_font'><i class='fas fa-user-circle'></i> About Me</h3><hr>";
+	page_html += "<div class='row raleway_font' style='padding-left: 5%; padding-right: 5%;'>";
+
+	// Education part
+	page_html += "<div class='col-md-6'>";
+	page_html += "<strong><i class='fas fa-user-graduate'></i> EDUCATION</strong>";
+	page_html += "<ul>";
+	var education_details = tab_data[0].data[0].data;
+	for(var i=0; i<education_details.length; i++){
+		page_html += "<li>";
+		page_html += education_details[i].programme+"<br>";
+		page_html += "<span style='font-size: 0.8em;'>"+education_details[i].institution+"<br>"+education_details[i].timeline+"</span><br>";
+		page_html += "</li>";
+	}
+	page_html += "</ul>";
+	page_html += "</div>";
+
+	// Interests part
+	page_html += "<div class='col-md-6'>";
+	page_html += "<strong><i class='fas fa-basketball-ball'></i> INTERESTS</strong>";
+	page_html += "<ul>";
+	var interests_details = tab_data[0].data[1].data;
+	for(var i=0; i<interests_details.length; i++){
+		page_html += "<li>"+interests_details[i].title+"</li>";
+	}
+	page_html += "</ul>";
+
+	// Languages Part
+	page_html += "<strong><i class='fas fa-language'></i> LANGUAGES</strong>";
+	page_html += "<ul>";
+	var languages_details = tab_data[0].data[2].data;
+	for(var i=0; i<languages_details.length; i++){
+		page_html += "<li>"+languages_details[i].title+"<br><p class='text-muted'>"+languages_details[i].type+"</p></li>";
+	}
+	page_html += "</ul>";
+	page_html += "</div>";
+
+	page_html += "</div></div>";
+	$("#inject").html(page_html);
+}
+
+/*
+	EXPERIENCE PAGE BUILDER
+*/
+
+function build_experience_page(){
+	var page_html = "<h3 class='raleway_font'><i class='fas fa-users'></i> Experiences</h3><hr><div class='container-fluid' style='overflow-y: scroll;'>";
+	page_html += "<div class='row'>";
+	var experience = tab_data[2].data;
+	for(var i=0; i<experience.length; i++){
+		page_html += "<div class='col-md-3'>";
+		page_html += "<button id='"+i+"' class='btn btn-default experience' style='background-color: rgba(0,0,0,0);' data-toggle='modal' data-target='#myModal' alt='"+i+"'>";
+		page_html += "<div class='con'>";
+		page_html += "<img src='img/"+experience[i].img+"' alt='"+experience[i].title+"' width='100%' height='100%' class='image'>";
+		page_html += "<span class='middle raleway_font'><strong>"+experience[i].title+"<strong></span>"
+		page_html += "</div>";
+		page_html += "</button>";
+		page_html += "</div>";
+	}
+	page_html += "</div>";
+	$("#inject").html(page_html);
+	$(".experience").click(make_experience_modal);
+}
+
+$(".experience").click(make_experience_modal);
+function make_experience_modal(){
+	var id = $(this).attr("id");
+	var experience_details = tab_data[2].data[id];
+	$("#modal_heading").html(experience_details.title);
+	var body_html = "";
+	body_html += "<div style='width:100%;text-align:center;'><img src='img/"+experience_details.img+"' width='50%' height='auto'></div><br>";
+	body_html += "<hr><strong>Position: </strong>"+experience_details.pos;
+	body_html += "<br><strong>Timeline: </strong>"+experience_details.timeline;
+	body_html += "<hr><strong>Description</strong><p style='text-align: justify;'>"+experience_details.text+"</p>";
+	$("#modal_body").html(body_html);
 }
---
+++ js/data.js
@@ -0,0 +1,233 @@
+/*
+	DATA
+*/
+var tab_data = [
+	{name: "about", data: [
+		{
+			name: "education", data: [
+				{
+					programme: "B.E. Electronics & Communications",
+					institution: "Thapar Institute of Engineering & Technology",
+					timeline: "2015 - Present"
+				},
+				{
+					programme: "Class XII",
+					institution: "Modern Public School, Bhiwadi",
+					timeline: "2014 - 2015"
+				},
+				{
+					programme: "Class X",
+					institution: "Modern Public School, Bhiwadi",
+					timeline: "2012 - 2013"
+				}
+			]
+		},
+		{
+			name: "interests", data: [
+				{
+					title: "Technology & Gadgets"	
+				},
+				{
+					title: "Mentoring Juniors"
+				},
+				{
+					title: "Swimming"	
+				},
+				{
+					title: "Chess"
+				}
+			]
+		},
+		{
+			name: "languages", data: [
+				{
+					title: "English",
+					type: "Professional"
+				},
+				{
+					title: "Hindi",
+					type: "Native"
+				}
+			]
+		}
+	]},
+	{name: "projects", data: [
+		{
+			title: "Project Sales Simulation",
+			img: "product_sales.png",
+			text: "This project aimed to create a real-life (almost) analysis of how a product would sell in an Indian market comprising various types of population. This was a helper project in my course Innovation & Entrepreneurship during semester 6 (Q1-2 2018). This python based project used Matplot Lib to plot the results obtained as day-wise & sales-wise graphs. Other than that it exports the data in a usable CSV format.Further details are listed at the GitHub link attached along.",
+			tech: "python",
+			timeline: "February 2018 – May 2018",
+			link: "https://github.com/shubham1chawla/Product-Simulation"
+		},
+		{
+			title: "The Strategists",
+			img: "thestrategists.png",
+			text: "The Strategists is a monopoly based online multiplayer game based on the concepts of Foreign Direct Investment. It involves buying, trading lands in percentages and deliver a fun gameplay. The project used PHP as the backend, JS with Bootstrap as frontend along with heavy use of AJAX.",
+			tech: "HTML, CSS, PHP, bootstrap",
+			timeline: "March 2018 – April 2018",
+			link: "http://thestrategists.000webhostapp.com"
+		},
+		{
+			title: "Room Manager",
+			img: "room_manager.png",
+			text: "Room Manager is a simple backend supported webapp that uses fundamentals of SQL, PHP and Bootstrap to implement a room managing app with ability to book rooms with different user levels.",
+			tech: "HTML, CSS, PHP, bootstrap",
+			timeline: "January 2018 – February 2018",
+			link: "https://github.com/shubham1chawla/roomManager"
+		},
+		{
+			title: "Tennis Game",
+			img: "tennis_game.jpg",
+			text: "Tennis Game uses core JAVA to simulate an air-hockey sort of game. CORE java used to handle different threads and other game physics to give realistic gameplay.",
+			tech: "JAVA",
+			timeline: "July 2017 – August 2017",
+			link: "https://github.com/shubham1chawla/Tennis_Game"
+		},
+		{
+			title: "Tic Tac Toe",
+			img: "tictactoe.png",
+			text: "This project was my first step to game development. Implementing TicTacToe gameplay on Terminal with a smart Computer AI that forms dynamic move-table in order to play against you. Project emphasis on how the computer will react to users moves and also predict where to place its turn using complex algorithms.",
+			tech: "JAVA",
+			timeline: "May 2017 – June 2017",
+			link: "https://github.com/shubham1chawla/TicTacToe"
+		}
+	]},
+	{name: "experiences", data: [
+		{
+			title: "CEC member, T.I.E.T.A.A",
+			img: "tietaa.jpg",
+			text: "CEC is an acronym for Central Executive Committee. Being a CEC member I was exposed to a vast number of Thapar Institute's alumni. Not only I organised alumni meets but to also I managed a huge database of our alumni network that Thapar Institute never fail to boast. Being president of Student Alumni Interaction Cell and CEC member brings in their own set of perks along with the opportunity to meet such great alumni. Under their guidance, the amount of opportunity you are exposed to is just infinite.",
+			pos: "Central Executive Committee member, Thapar Institute of Engineering & Technology Alumni Association",
+			timeline: "January 2018 – Present"
+		},
+		{
+			title: "President SAIC, T.I.E.T.",
+			img: "saic.jpg",
+			text: "\"With great power comes great responsibilities\", A famous quote once said. But is it only powers that we are talking? Well, it doesn't, at least my journey in SAIC proved me otherwise. From a mere team member to the president of this cell, I have grown a lot when it comes to dealing with real-life challenges. Many of them come from my experience in SAIC. I have been a part of Student Alumni Interaction Cell since my 2nd semester in B.E programme. Most of my soft skills developed during my journey here. But being a president means not just to learn but to make everyone with you grow along.  And that's when great leaders are born. Like my seniors, I like to help my juniors and work alongside them. Another soft skill that I encounter was to accept your mistakes. I know we humans are made to be perfect, but everyone has their flaws. <br>Appart from the skills that I did grow upon, there were few achievements along the way as well. With the help of my coordinators, we were able to pull up great events like Troubleshoot and Global Leadership Summit 2018 at such a magnificent scale. For me, being the president is just a fancy tag that comes with great responsibilities to fulfil.",
+			pos: "President, Studen Alumni Interaction Cell, Thapar Institute",
+			timeline: "December 2017 – Present"
+		},
+		{
+			title: "O.E.C. Saturnalia 2017",
+			img: "sat.jpg",
+			text: "Being overall event coordinator of a single event brings a lot of changes in you. You become a better version of yourself. But being OEC of a fest is taking that learning curve to a whole new level. In a fest like Saturnalia, where every day brings up new events, one could just imagine how things are sorted out in such hassle.<br>My journey as OEC started off in April 2017. As OEC, I came across many hurdles, one of them being to maintain interdepartmental synchronization and proper flow of tasks amongst them. It requires judicious managemental skill to overcome such scenarios. The most important thing that I learned during my journey was the ability to take charge of things and to lead a team with a common agenda. For me, learning along made this experience worth sharing.",
+			pos: "Overall Event Coordinator, Saturnalia 2017 (Annual Techno-Cultural Fest, Thapar Institute)",
+			timeline: "April 2017 – November 2017"
+		},
+		{
+			title: "O.E.C. Global Leadership Summit",
+			img: "gls.jpg",
+			text: "Global Leadership Summit was the life-changing event for me. Not only it was my key to success but, it shifted my perspective on how I should deal with my fellow team members. When my society presidents appointed me as OEC of GLS 2017, at first it seemed like everyone has to just blindly follow my lead. But being just appointed to be a leader doesn't make you one. I learned that the hard way when everything seems to fall apart. Luckily, my seniors helped me during my fall. They not only taught me the importance of working in a team but also focused on respecting each and everyone's opinion.<br>Working in a team means you should trust them. This was yet another soft skill that I developed during my time as OEC. In the end, it was 'we' who pulled off a successful event and not just 'I'.",
+			pos: "Overall Event Coordinator, Global Leadership Summit 2017",
+			timeline: "February 2017 – April 2017"
+		},
+		{
+			title: "O.E.C. Great Britain Debate 2017",
+			img: "gbd.jpg",
+			text: "Great Britain Debate was a wildcard opportunity for me. I was offered to be one of the coordinators of the event just few weeks before the event.  As great as the opportunity, it was a challenge for me to cope up with event's preparation. During my journey as one of the coordinator, I managed many of the logistic preparation and even designed graphics for online publicity.<br>After seeing an event that was organised by British Commission, New Delhi itself, the level of detail we focused were on par. As the saying goes, \"The most important tactic in an argument, next to being right, is to leave an escape hatch for your opponent, so that he can gracefully swing over to your side without an embarrassing loss of face.\", we should all be respectful in our fights as well.",
+			pos: "Overall Event Coordinator, Great Britain Debate 2017 (conducted by the British High Commission, New Delhi)",
+			timeline: "December 2017 – January 2017"
+		},
+		{
+			title: "Media & Branding Head",
+			img: "connectu.jpg",
+			text: "As a freshman, first, real responsibility comes from your urge to prove yourself. This was the time when I was given a great opportunity to revive a long-dead newsletter that Student Alumni Interaction Cell used to print earlier in days. Though I had my experience in graphic designing earlier in my freshman days, designing a regularly publishing newsletter was a challenge I never faced before. It was my urge to prove myself which pushed me to my bounds and enabled me to reach our target.<br>Link to the newsletter I designer is <u><a href='https://drive.google.com/open?id=1wCBdwkIUecFbCklYHCLdhrWOHMjG6oCQ'>here.</a></u>",
+			pos: "Media & Branding Head, Student Alumni Interaction Cell",
+			timeline: "October 2016 – April 2017"
+		},
+		{
+			title: "Designing Head, Saturnalia 2016",
+			img: "sat16.jpg",
+			text: "I wouldn't have been able to design my own portfolio if it wasn't for Saturnalia 2016. It provided me with the opportunity to be the Designing Head, in days when the positions of heads were reserved for senior years, I was able to achieve mine just after the first year.<br>My journey was filled up with regular updating of publicity material, anything remotely graphical. With so frequent demand for designs including posters, flexes and banners and focusing on my studies as well taught me how to manage time and devote it to things that were of absolute priorities.",
+			pos: "Designing Head, Saturnalia 2016 (Annual Techno-Cultural Fest, Thapar Institute)",
+			timeline: "April 2016 – October 2017"
+		}
+	]},
+	{name: "skills", data: [
+		{
+			title: "HTML 5",
+			icon: "<i class='fab fa-html5 fa-5x'></i>",
+			level: 5
+		},
+		{
+			title: "CSS 3",
+			icon: "<i class='fab fa-css3-alt fa-5x'></i>",
+			level: 3
+		},
+		{
+			title: "JavaScript",
+			icon: "<i class='fab fa-js-square fa-5x'></i>",
+			level: 4
+		},
+		{
+			title: "Bootstrap",
+			icon: "<img src='img/B_black_white.svg' width='80px' height='80px' style='border-radius: 10%; opacity: 0.9;'>",
+			level: 4
+		},
+		{
+			title: "Jquery",
+			icon: "<img src='img/jquery-filled.png' width='80px' height='80px; opacity: 0.9;'>",
+			level: 3
+		},
+		{
+			title: "PHP",
+			icon: "<i class='fab fa-php fa-5x'></i>",
+			level: 4
+		},
+		{
+			title: "MySQL",
+			icon: "<img src='img/mysql-logo-filled.png' width='80px' height='80px'; opacity: 0.9;>",
+			level: 5
+		},
+		{
+			title: "C++",
+			icon: "<img src='img/c-plus-plus-logo-filled.png' width='80px' height='80px; opacity: 0.9;'>",
+			level: 4
+		},
+		{
+			title: "Python",
+			icon: "<i class='fab fa-python fa-5x'></i>",
+			level: 3
+		},
+		{
+			title: "JAVA",
+			icon: "<i class='fab fa-java fa-5x'></i>",
+			level: 3
+		},
+		{
+			title: "ARMv7",
+			icon: "<i class='fas fa-microchip fa-5x'></i>",
+			level: 3
+		},
+		{
+			title: "Adobe Photoshop",
+			icon: "<img src='img/ps.jpg' width='80px' height='80px; opacity: 0.9;'>",
+			level: 5
+		},
+		{
+			title: "Adobe Illustrator",
+			icon: "<img src='img/ai.jpg' width='80px' height='80px; opacity: 0.9;'>",
+			level: 4
+		},
+		{
+			title: "Adobe Premiere Pro",
+			icon: "<img src='img/ppro.png' width='80px' height='80px; opacity: 0.9;'>",
+			level: 4
+		},
+		{
+			title: "Adobe Indesign",
+			icon: "<img src='img/in.jpg' width='80px' height='80px; opacity: 0.9;'>",
+			level: 4
+		}
+	]},
+	{name: "taglines", data: [
+		{name: "I'm a Full Stack Developer"},
+		{name: "I'm a UI/UX Designer"},
+		{name: "I <i class='fas fa-heart'></i> Food"},
+		{name: "I'm a proud Indian"},
+		{name: "Particle.js is cool af!"},
+		{name: "9gag? 9GAG!"},
+		{name: "I'm a Freelancer"}
+	]}
+];
