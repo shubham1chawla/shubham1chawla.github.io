37102df9d85d796e2f668546fb66dafb955f55c8
Shubham Chawla <shubham_chawla@live.com>
2024-06-05 16:55:54 -0700

    added internship chat

--- js/main.js
+++ js/main.js
@@ -44,3 +44,60 @@ document.querySelectorAll('.slide').forEach((slide) => {
         window.open(url, '_blank');
     });
 });
+
+// Adding typing animation
+const simulateChat = async (bubbles) => {
+    
+    // Removing innerHTML for all the elements at start
+    const map = new Map();
+    bubbles.forEach((bubble) => {
+        bubble.classList.add('animate', 'animate-out')
+        const body = bubble.querySelector('.chat-bubble__body');
+        const { innerHTML: html, textContent: text } = body;
+        map.set(body, [html, text.trim().replace(/\s\s+/g, ' ')]);
+        body.innerHTML = '';
+    });
+
+    // Typing text inside elements one at a time
+    for (const bubble of bubbles) {
+        bubble.classList.replace('animate-out', 'animate-in');
+        const body = bubble.querySelector('.chat-bubble__body');
+        const [html, text] = map.get(body);
+        index = 0
+
+        // Blinking caret span element
+        const caret = document.createElement('span')
+        caret.className = 'caret';
+        caret.innerHTML = '|';
+        bubble.appendChild(caret);
+
+        const typeText = (resolve) => {
+            if (index < text.length) {
+                body.innerHTML += text.charAt(index++);
+                const timeout = Math.ceil(40 + 2 * Math.random());
+                setTimeout(() => typeText(resolve), timeout);
+            } else {               
+                resolve();
+            }
+        }
+        await new Promise((resolve) => typeText(resolve));
+ 
+        // Setting original innerHTML to resolve anchor tags
+        body.innerHTML = html;
+        bubble.removeChild(caret);
+    }
+};
+
+// Setting up chat intersection observer
+const chatObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const bubbles = document.querySelector('.chat').querySelectorAll('.chat-bubble');
+            simulateChat(bubbles);
+            fadeObserver.unobserve(entry.target);
+        }
+    });
+});
+
+// Adding main chat container to the observer
+chatObserver.observe(document.querySelector('main.chat'));
