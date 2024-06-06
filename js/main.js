4e1ac37afe475be4a5c1852b4b0df5e2acc129e5
Shubham Chawla <shubham_chawla@live.com>
2024-06-05 19:02:43 -0700

    fix height and overflow issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Adding on click for scroll down button
 document.getElementById('scroll-down').addEventListener('click', () => {
-    const height = document.documentElement.clientHeight - 100;
+    const { height } = document.querySelector('section.landing').getClientRects()[0];
     window.scrollTo({ top: height, behavior: 'smooth' });
 });
 
@@ -14,8 +14,8 @@ const fadeObserver = new IntersectionObserver((entries) => {
     });
 });
 
-// Adding animatable elements to the observer
-document.querySelectorAll('.animate-out').forEach((element) => fadeObserver.observe(element));
+// Adding header elements to the observer
+document.querySelectorAll('header.animate-out').forEach((element) => fadeObserver.observe(element));
 
 // Setting up slide intersection observer
 const slideObserver = new IntersectionObserver((entries) => {
@@ -47,11 +47,13 @@ document.querySelectorAll('.slide').forEach((slide) => {
 
 // Adding typing animation
 const simulateChat = async (bubbles) => {
-    
-    // Removing innerHTML for all the elements at start
+
+    // Storing innerHTML and textContent for bot's chat bubbles
     const map = new Map();
     bubbles.forEach((bubble) => {
-        bubble.classList.add('animate', 'animate-out')
+        if (bubble.classList.contains('chat-bubble-user')) {
+            return;
+        }
         const body = bubble.querySelector('.chat-bubble__body');
         const { innerHTML: html, textContent: text } = body;
         map.set(body, [html, text.trim().replace(/\s\s+/g, ' ')]);
@@ -60,28 +62,36 @@ const simulateChat = async (bubbles) => {
 
     // Typing text inside elements one at a time
     for (const bubble of bubbles) {
-        bubble.classList.replace('animate-out', 'animate-in');
         const body = bubble.querySelector('.chat-bubble__body');
-        const [html, text] = map.get(body);
-        index = 0
+        bubble.classList.replace('animate-out', 'animate-in');
+
+        // Adding delay before next bot's chat bubble appears
+        if (bubble.classList.contains('chat-bubble-user')) {
+            await new Promise((resolve) => {
+                setTimeout(resolve, 1000);
+            });
+            continue;
+        }
 
         // Blinking caret span element
-        const caret = document.createElement('span')
+        const caret = document.createElement('span');
         caret.className = 'caret';
         caret.innerHTML = '|';
         bubble.appendChild(caret);
 
+        const [html, text] = map.get(body);
+        index = 0;
         const typeText = (resolve) => {
             if (index < text.length) {
                 body.innerHTML += text.charAt(index++);
-                const timeout = Math.ceil(40 + 2 * Math.random());
+                const timeout = Math.ceil(20 + 30 * Math.random());
                 setTimeout(() => typeText(resolve), timeout);
-            } else {               
+            } else {
                 resolve();
             }
         }
         await new Promise((resolve) => typeText(resolve));
- 
+
         // Setting original innerHTML to resolve anchor tags
         body.innerHTML = html;
         bubble.removeChild(caret);
@@ -92,12 +102,18 @@ const simulateChat = async (bubbles) => {
 const chatObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            const bubbles = document.querySelector('.chat').querySelectorAll('.chat-bubble');
-            simulateChat(bubbles);
-            fadeObserver.unobserve(entry.target);
+            simulateChat(document.querySelectorAll('.chat-bubble'));
+            chatObserver.unobserve(entry.target);
         }
     });
 });
 
+// Setting minimum height for the chat's main element
+const chatElement = document.querySelector('section.internship>main');
+const styles = getComputedStyle(chatElement);
+const height = styles.height;
+const margin = styles.getPropertyValue('--large-unit');
+chatElement.setAttribute('style', `min-height: calc(${height} + ${margin})`);
+
 // Adding main chat container to the observer
-chatObserver.observe(document.querySelector('main.chat'));
+chatObserver.observe(chatElement);
